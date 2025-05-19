1496f820b6a38057ccf7d90680edc710cf480c23
dodo <palumbododo@gmail.com>
2025-05-19 15:11:26 +0200

    aa

--- experiment2.py
+++ experiment2.py
@@ -3,7 +3,7 @@
 from accuracy_test import accuracy
 import time
 import tensorflow as tf
-import keras
+from tensorflow import keras
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
 physical_devices = tf.config.list_physical_devices('GPU')
