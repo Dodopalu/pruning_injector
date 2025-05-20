9936f05051645422167492e72cc9f0d2690191f4
dodo <palumbododo@gmail.com>
2025-05-20 15:40:42 +0200

    aa

--- pruning_experiment.py
+++ pruning_experiment.py
@@ -4,6 +4,11 @@ from loaderCIFAR10 import load
 from pruning_structural import structural_pruning
 from convert_to_TensorRT import convert_to_TensorRT
 
+train, test = load()
+dataset = train
+
+
+'''
 # CIFAR10 models
 densenet121 = "./models_tensorRT/CIFAR10/DenseNet121"
 densenet121_BASE = "./models/CIFAR10/densenet/DenseNet121.keras"
@@ -20,12 +25,9 @@ convert_to_TensorRT(
 
 densenet121_tensorRT = "./models_tensorRT/CIFAR10/structural_2_4"
 
-train, test = load()
-dataset = train
-
 benchmark_trt(densenet121, dataset, batch=64)
 benchmark_trt(densenet121_tensorRT, dataset, batch=64)
-
+'''
 
 
 BASE = "./models_pd/CIFAR10/DenseNet121"
