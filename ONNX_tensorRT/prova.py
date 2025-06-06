15658aab711ab4e862cdb477e227d358dec91bf7
dodo <palumbododo@gmail.com>
2025-06-06 16:23:28 +0200

    aa

--- ONNX_tensorRT/prova.py
+++ ONNX_tensorRT/prova.py
@@ -7,6 +7,7 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 
 test = np.zeros((64,32,32,3))  # Reshape to (batch_size, channels, height, width)
+test[0, 0, 0, 0] = 1.0  # Example of setting a value in the array
 print(test.nbytes)
 
 
@@ -14,7 +15,7 @@ input_ptr = cuda.mem_alloc(test.nbytes)
 cuda.memcpy_htod(input_ptr, test)
 
 
-out = None
+out = np.array(np.zeros((64, 10)), dtype=np.float32)  # Assuming output shape is (batch_size, num_classes)
 cuda.memcpy_dtoh(input_ptr, out)
 
 print(out.shape)
