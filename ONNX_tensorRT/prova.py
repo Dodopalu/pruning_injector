1422ba980ff724cc9274a913504e0bf1ec907255
dodo <palumbododo@gmail.com>
2025-06-06 15:46:52 +0200

    ss

--- ONNX_tensorRT/prova.py
+++ ONNX_tensorRT/prova.py
@@ -27,8 +27,18 @@ test = test.batch(64).take(1)
 test = test.as_numpy_iterator()[0]
 test = test.ravel()
 
+print(test.shape)  # Should be (64, 32, 32, 3)
+print(test.dtype)  # Should be float32
 
 input_ptr = cuda.mem_alloc(test.nbytes)
 cuda.memcpy_htod(input_ptr, test)
 
 output = np.empty((64, 10), dtype=np.float32).ravel()
+
+output_ptr = cuda.mem_alloc(output.nbytes)
+
+bindings = [int(input_ptr), int(output_ptr)]
+
+
+with engine.create_execution_context() as context:
+    context.execute_v2(bindings=bindings)
